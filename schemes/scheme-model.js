0247edef9e74975c1a4f0d5ec2a1d2adba65d825
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-11 14:39:56 -0600

    created first helper function find

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,7 +10,7 @@ module.exports = {
 }
 
 function find() {
-
+    return db('schemes')
 }
 
 function findById(id) {
--- schemes/scheme-router.js
+++ schemes/scheme-router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   Schemes.find()
   .then(schemes => {
-    res.json(schemes);
+    res.status(200).json(schemes);
   })
   .catch(err => {
     res.status(500).json({ message: 'Failed to get schemes' });
