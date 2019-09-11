ee93a91c0fa2c3da3712271f411e9fe949f78045
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-11 14:30:21 -0600

    created skelleton for all helper functions in scheme-model.js

---
+++ schemes/scheme-model.js
@@ -0,0 +1,34 @@
+const db = require('../data/db-config')
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+
+}
+
+function findById(id) {
+
+}
+
+function findSteps(id) {
+
+}
+
+function add(scheme) {
+
+}
+
+function update(id, changes) {
+
+}
+
+function remove(id) {
+
+}
